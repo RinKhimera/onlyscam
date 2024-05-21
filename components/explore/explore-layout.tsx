aef5b856395fae3f0b091e62bec8ba6abec81e07
Samuel Pokam <dixiades@gmail.com>
2024-05-21 15:15:43 +0100

    pawapay test

--- components/explore/explore-layout.tsx
+++ components/explore/explore-layout.tsx
@@ -1,15 +1,49 @@
 "use client"
 
 import { CldUploadWidget } from "next-cloudinary"
+import { useRouter } from "next/navigation"
+import { v4 as uuidv4 } from "uuid"
+import { Button } from "../ui/button"
 
-export const ExploreLayout = () => {
+export const ExploreLayout = async () => {
+  const router = useRouter()
+
+  const fetchData = async () => {
+    const depositId = uuidv4()
+
+    const resp = await fetch(
+      `https://api.sandbox.pawapay.cloud/v1/widget/sessions`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_PAWAPAY_SANDBOX_API_TOKEN}`,
+        },
+        body: JSON.stringify({
+          depositId: depositId,
+          returnUrl: "https://onlyscam.vercel.app/notifications",
+          statementDescription: "Note of 4 to 22 chars",
+          amount: "100",
+          msisdn: "233593456789",
+          country: "ZMB",
+          reason: "Ticket to festival",
+        }),
+      },
+    )
+
+    const data = await resp.json()
+    console.log(data, depositId)
+
+    // router.push(data.redirectUrl)
+  }
+
   return (
     <main className="flex h-full min-h-screen w-[50%] flex-col border-l border-r border-muted max-lg:w-[80%]">
       <h1 className="sticky top-0 z-20 border-b border-muted p-4 text-2xl font-bold backdrop-blur">
         Explorer
       </h1>
 
-      <CldUploadWidget
+      {/* <CldUploadWidget
         uploadPreset="post-videos"
         signatureEndpoint="/api/sign-cloudinary-params"
         options={{
@@ -28,7 +62,9 @@ export const ExploreLayout = () => {
             </button>
           )
         }}
-      </CldUploadWidget>
+      </CldUploadWidget> */}
+
+      <Button onClick={fetchData}>Payer</Button>
     </main>
   )
 }
--- components/notifications/notification-layout.tsx
+++ components/notifications/notification-layout.tsx
@@ -1,9 +1,35 @@
+"use client"
+
+import { useSearchParams } from "next/navigation"
+import { Button } from "../ui/button"
+
 export const NotificationsLayout = () => {
+  const searchParams = useSearchParams()
+  const depositId = searchParams.get("depositId")
+
+  const fetchData = async () => {
+    const resp = await fetch(
+      `https://api.sandbox.pawapay.cloud/deposits/${depositId}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_PAWAPAY_SANDBOX_API_TOKEN}`,
+        },
+      },
+    )
+
+    const data = await resp.text()
+    console.log(data, depositId)
+  }
+
   return (
     <main className="flex h-full min-h-screen w-[50%] flex-col border-l border-r border-muted max-lg:w-[80%]">
       <h1 className="sticky top-0 z-20 border-b border-muted p-4 text-2xl font-bold backdrop-blur">
         Notifications
       </h1>
+
+      <Button onClick={fetchData}>Recevoir statut</Button>
     </main>
   )
 }
