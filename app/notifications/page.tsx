6eea99dd466ead493b2251935746dc5a95ecba18
Samuel Pokam <dixiades@gmail.com>
2024-05-27 23:17:05 +0100

    Update page.tsx

--- app/notifications/page.tsx
+++ app/notifications/page.tsx
@@ -1,16 +1,18 @@
+"use client"
+
 import { NotificationsLayout } from "@/components/notifications/notification-layout"
 import { LeftSidebar } from "@/components/shared/left-sidebar"
 import { RightSidebar } from "@/components/shared/right-sidebar"
-// import { Suspense } from "react"
+import { Suspense } from "react"
 
 const NotificationsPage = () => {
   return (
     <div className="relative flex h-full w-full items-center justify-center">
       <div className="relative flex h-full w-full max-w-screen-xl">
         <LeftSidebar />
-        {/* <Suspense> */}
-        <NotificationsLayout />
-        {/* </Suspense> */}
+        <Suspense>
+          <NotificationsLayout />
+        </Suspense>
         <RightSidebar />
       </div>
     </div>
