8ebb657bd7817123b1a843a3b5cbeb9e4f2695dd
Samuel Pokam <dixiades@gmail.com>
2024-06-12 18:00:02 +0100

    Update user-collections.tsx

--- components/collections/user-collections.tsx
+++ components/collections/user-collections.tsx
@@ -39,7 +39,7 @@ export const UserCollections = ({
             >
               <div className="flex w-full flex-col">
                 <div className="flex items-center justify-between">
-                  <Link href={`/${post.author?.username}`}>
+                  <Link href={`/${post?.author?.username}`}>
                     <div className="flex items-center gap-3">
                       <Avatar>
                         <AvatarImage
